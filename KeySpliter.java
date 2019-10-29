48615b65b8ca4b8100990f9ec690ad8327b4766f
k670 <2ki15b.melnik@gmail.com>
2019-10-29 15:31:01 +0200

    Add files via upload

--- KeySpliter.java
+++ KeySpliter.java
@@ -1,6 +1,8 @@
 package com.playtika.maas;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -9,19 +11,22 @@ public class KeySpliter {
     public static void main(String[] args) {
         Map<String, Object> oldMap = new HashMap<String, Object>() {{
             put("key12.key22.key33.key41", 41);
-            put("key12.key22.key33", 33);
-            put("key12.key22.key32", 32);
-            put("key12.key22.key31", 31);
-            put("key12.key22.key33.key44.key55", 55);
+            put("key12.key22.key33", new HashMap<String, Object>() {{
+                put("key44", 44);
+            }});
         }};
+        System.out.println("old");
+        System.out.println(oldMap);
 
         Map<String, Object> newMap = splitKey(oldMap);
+        System.out.println("new");
         System.out.println(newMap);
 
     }
 
     private static Map<String, Object> splitKey(Map<String, Object> map) {
         Map<String, Object> resMap = new HashMap<>();
+        List<KeyValueMapClass> keyValueMapList = new ArrayList<>();
         map.forEach((k, v) -> {
             String[] splitStr = k.split(Pattern.quote("."));
             int length = splitStr.length - 1;
@@ -31,7 +36,7 @@ public class KeySpliter {
                     lastMap = (Map<String, Object>) lastMap.get(splitStr[i]);
                 } else {
                     if (i == length) {
-                        lastMap.put(splitStr[i], v);
+                        keyValueMapList.add(new KeyValueMapClass(splitStr[i], v, lastMap));
                     } else {
                         Map<String, Object> newmap = new HashMap<>();
                         lastMap.put(splitStr[i], newmap);
@@ -40,7 +45,23 @@ public class KeySpliter {
                 }
             }
         });
+        keyValueMapList.forEach(kVMap -> {
+            kVMap.map.merge(kVMap.key, kVMap.value, (a, b) -> a);
+        });
         return resMap;
+    }
+
+}
+
+class KeyValueMapClass {
+    String key;
+    Object value;
+    Map<String, Object> map;
+
+    KeyValueMapClass(String key, Object value, Map<String, Object> map) {
+        this.key = key;
+        this.map = map;
+        this.value = value;
     }
 
 }

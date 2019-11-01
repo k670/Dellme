cbbeeed2d2ff5dd13cec5edcfc6dfb3cebbc1892
k670 <2ki15b.melnik@gmail.com>
2019-11-01 18:34:47 +0200

    Add files via upload

---
+++ JsonNodeSave.java
@@ -0,0 +1,98 @@
+package com.playtika.maas.mapstruct;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class JsonNodeSave {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        Map<String, Object> map = new HashMap<String, Object>() {{
+            put("a.b.c.k1", 123L);
+            put("a.b.k2", "v2");
+            put("a.b.c.k3", Arrays.asList(1, 2, 3, 4, 5));
+            put("a.b.c.k4", new int[][]{{6, 7, 8, 9, 0}, {11, 22, 33, 44}});
+            put("a.tt", new SimpleDestination());
+            put("a.tt.name", "new name");
+        }};
+        String JACKSONSEPARATOR = "/";
+
+        map.forEach((address, value) -> {
+
+            putOnNode(objectNode, JACKSONSEPARATOR, address, value);
+
+            try {
+                System.out.println(objectMapper.treeToValue(objectNode, Map.class));
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
+    }
+
+    private static void putOnNode(ObjectNode objectNode, String JACKSONSEPARATOR, String address, Object value) {
+        List<String> addressList = Arrays.asList(address.split("\\."));
+        int lastIndex = addressList.size() - 1;
+        String key = addressList.get(lastIndex);
+        String firstAddress = addressList.get(0);
+        addressList = addressList.subList(1, lastIndex);
+
+        JsonNode existingNode = objectNode.at(JACKSONSEPARATOR + firstAddress);
+        ObjectNode lastObjectNode;
+        int size = addressList.size();
+        if (existingNode.getNodeType() == JsonNodeType.OBJECT) {
+            lastObjectNode = (ObjectNode) existingNode;
+            for (int j = 0; j < size; j++) {
+                existingNode = lastObjectNode.at(JACKSONSEPARATOR + addressList.get(j));
+                if (existingNode.getNodeType() != JsonNodeType.OBJECT) {
+                    lastObjectNode = createJsonNodes(addressList.subList(j, size), lastObjectNode);
+                    break;
+                }
+                lastObjectNode = (ObjectNode) existingNode;
+            }
+        } else {
+            lastObjectNode = objectMapper.createObjectNode();
+            objectNode.set(firstAddress, lastObjectNode);
+            lastObjectNode = createJsonNodes(addressList, lastObjectNode);
+        }
+
+
+        setOnNode(value, key, lastObjectNode);
+    }
+
+    private static ObjectNode createJsonNodes(List<String> createAddressList, ObjectNode lastObjectNode) {
+        for (String adrs : createAddressList) {
+            ObjectNode newObjNode = objectMapper.createObjectNode();
+            lastObjectNode.set(adrs, newObjNode);
+            lastObjectNode = newObjNode;
+        }
+        return lastObjectNode;
+    }
+
+    private static void setOnNode(Object value, String key, ObjectNode lastObjectNode) {
+        Class<?> valueClass = value.getClass();
+        if (valueClass.isPrimitive()) {
+            lastObjectNode.putPOJO(key, value);
+        } else {
+            JsonNode jsonNode = objectMapper.valueToTree(value);
+            if (valueClass.isArray()) {
+                lastObjectNode.putArray(key).addAll((ArrayNode) jsonNode);
+            } else {
+                lastObjectNode.set(key, jsonNode);
+            }
+        }
+    }
+}
